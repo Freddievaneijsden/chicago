3e7d1dad3e21333953627f081686fc2dd594b0e2
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-10-13 16:46:48 +0200

    Extract revert-score to .js component

---
+++ docs/components/RevertScore.js
@@ -0,0 +1,8 @@
+export const RevertScore = {
+props: ["currentPhase", "phases", "lastScore"],
+template: `<div class="btnWrapper">
+                      <button @click="$emit('revert-last-score')" :disabled="!lastScore" class="startBtn">
+                        Revert
+                      </button>
+                     </div>`,
+        }
